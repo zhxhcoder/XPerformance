1c7151e347949f8cea39054ef2bc3dc8fd6fd1a0
zhxh <zhxhcoder@gmail.com>
2023-04-07 11:28:00 +0800

    Initial commit

---
+++ app/src/main/java/com/zhxh/performance/DashActivity.kt
@@ -0,0 +1,11 @@
+package com.zhxh.performance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash)
+    }
+}
