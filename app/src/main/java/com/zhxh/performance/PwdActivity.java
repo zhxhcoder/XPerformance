d4a089930f666d7e648d7b666aecb2cf9e5e66c4
zhxh <zhxhcoder@gmail.com>
2023-04-25 08:43:30 +0800

    pwd测试

---
+++ app/src/main/java/com/zhxh/performance/PwdActivity.java
@@ -0,0 +1,14 @@
+package com.zhxh.performance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwd);
+    }
+}
