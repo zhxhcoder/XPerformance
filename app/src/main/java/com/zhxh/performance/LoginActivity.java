849189cc1600fe4d505279a7165bb0baf98603c4
zhxh <zhxhcoder@gmail.com>
2023-04-25 11:00:17 +0800

    editText

--- app/src/main/java/com/zhxh/performance/LoginActivity.java
+++ app/src/main/java/com/zhxh/performance/LoginActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.zhxh.performance.support.StringUtil;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -19,11 +24,34 @@ public class LoginActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         pwd = findViewById(R.id.pwd);
 
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //移除监听
+                name.removeTextChangedListener(this);
+                name.setText(StringUtil.replace("(?<=^.).", "*", s.toString()));
+                name.setSelection(s.length());
+                //重新监听
+                name.addTextChangedListener(this);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //账号为abcd,密码为123
                 Toast.makeText(LoginActivity.this, name.getText().toString() + ":" + pwd.getText().toString(), Toast.LENGTH_LONG).show();
-                startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                startActivity(new Intent(LoginActivity.this, DashActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/zhxh/performance/support/StringUtil.java
@@ -0,0 +1,20 @@
+package com.zhxh.performance.support;
+
+/*
+ * Created by zhxh on 2023/4/25
+ */
+public class StringUtil {
+    public static boolean isEmpty(CharSequence s) {
+        return s == null || s.length() == 0;
+    }
+
+    public static String replace(String regex, String replacement, CharSequence s) {
+        if (isEmpty(regex)) {
+            return String.valueOf(s);
+        }
+        if (isEmpty(s)) {
+            return "";
+        }
+        return s.toString().replaceFirst(regex, replacement);
+    }
+}
