fc060d169afef2986569b94271a9a4290dcc356a
zhxh <zhxhcoder@gmail.com>
2023-05-16 15:34:26 +0800

    DebugJSActivity

--- app/src/main/java/com/zhxh/performance/DebugJSActivity.java
+++ app/src/main/java/com/zhxh/performance/DebugJSActivity.java
@@ -6,18 +6,13 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 public class DebugJSActivity extends AppCompatActivity {
-
     WebView wb;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_debug_jsactivity);
-
-        wb = findViewById(R.id.wb);
+        wb = new WebView(this);
+        setContentView(wb);
 
         wb.loadUrl("file:android_asset/www/debug_js_native.html");
-
-
     }
 }
